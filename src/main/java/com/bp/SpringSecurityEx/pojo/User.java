2df71f640a254bc553e9f05dd71d3fba017d8645
Bhanu Prakash <bpbhardwaj@Bhanus-MacBook-Air.local>
2025-07-20 10:04:36 +0530

    changes mysql to postgresql

--- src/main/java/com/bp/SpringSecurityEx/pojo/User.java
+++ src/main/java/com/bp/SpringSecurityEx/pojo/User.java
@@ -6,8 +6,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "application_users")
 public class User {
 	public List<String> getRoles() {
 		return roles;
